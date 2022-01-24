5fc9c1e79ce0cfffcd242673c9aaa29459ee7bfe
RomainMvd <romain.mvd@outlook.fr>
2022-01-24 22:54:05 +0100

    methode pour ajouter commentaires

--- src/main/java/com/inti/service/interfaces/IEvaluationService.java
+++ src/main/java/com/inti/service/interfaces/IEvaluationService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.inti.entities.Evaluation;
 
 public interface IEvaluationService {
-	
+
 	List<Evaluation> findAll();
 
 	Evaluation findOne(Long idEvaluation);
@@ -13,4 +13,8 @@ public interface IEvaluationService {
 	Evaluation save(Evaluation evaluation);
 
 	void delete(Long idEvaluation);
+
+	List<Evaluation> listCommentaires(String idEvaluation);
+
+	// Evaluation commenter(String commentaire, Double note_cours, Integer id_cours);
 }
